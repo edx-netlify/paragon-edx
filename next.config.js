02bb8e3d27e2f5b95c06964f1eaf36db9f272e1b
Adam Stankiewicz <agstanki@gmail.com>
2022-03-17 09:39:27 -0400

    fix: use next.js for redirects instead of netlify (#10)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://paragon-openedx.netlify.app/:path*',
+        permanent: true,
+      },
+    ];
+  },
 }
 
-const withTM = require('next-transpile-modules')(['@edx/paragon']); // pass the modules you would like to see transpiled
-
-module.exports = withTM(nextConfig);
+const withTM = require('next-transpile-modules')(['@edx/paragon']);// pass the modules you would like to see transpiled
+module.exports = withTM(nextConfig)
